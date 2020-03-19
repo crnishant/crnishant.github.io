a773f2a3361727f08c29613c60cf20ac1d306794
Nishant Chintala <crnishant@gmail.com>
2020-03-18 22:19:52 -0400

    worked on about me page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-$(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 1);
-});
+/* card elements on about me page fading in on scroll, partially
+working
+$(document).ready(function() {
+	$(window).scroll( function(){
+		$('.card-deck').each( function(i){
+			var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			if( bottom_of_window > bottom_of_element ){
+				$(this).animate({'opacity':'1'},10);
+			}
+ 		});
+	});
+});*/
+	
