0cc20721b713d48d558ef7295596d32416036120
Nishant Chintala <crnishant@gmail.com>
2020-03-24 19:50:18 -0400

    deleted unnecessary images

--- scraper.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-source = requests.get('https://www.wsj.com').text
-
-soup = BeautifulSoup(source, 'lxml')
-articleTitle = soup.find(class_ = 'wsj-headline-link').text
-print(articleTitle)
