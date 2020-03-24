b1a318fc9b75503759456c86c1e8595bf75dbbe3
Nishant Chintala <crnishant@gmail.com>
2020-03-24 15:54:59 -0400

    working card fade in on experiences

--- scripts.js
+++ scripts.js
@@ -10,27 +10,18 @@ $(window).scroll(function(){
 	}
 });
 
-const faders = document.querySelectorAll('.fade-in');
-const appearOptions = {
-	threshold:1,
-	rootMargin: "0px 0px -100px 0px"
-};
-const appearOnScroll = new IntersectionObserver(
-	function(
-		entries,
-		appearOnScroll
-	) {
-		entries.forEach(entry=> {
-			if(!entry.isIntersecting){
-				return;
-			} else{
-				entry.target.classList.add('appear');
-				appearOnScroll.unobserve(entry.target);
+/* Works, but causes some glitchiness with the navbar color
+$(document).ready(function() {
+	$(window).scroll(function() {
+		$('.fade-in').each( function(i) {
+			var objectBottom= $(this).offset().top + $(this).outerHeight();
+			var windowBottom= $(window).scrollTop() + $(window).height();
+			var offset=200;
+			if(windowBottom > objectBottom-offset) {
+				$(this).animate({
+					'opacity': '1'
+				}, 500);
 			}
 		});
-	}, 
-	appearOptions);
-
-faders.forEach(fader => {
-	appearOnScroll.observe(fader);
-})
+	});
+});*/
